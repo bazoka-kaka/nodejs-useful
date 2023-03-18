73a9126295048bd86f06e33d77ea1447a0a8fb52
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2023-03-18 15:37:26 +0700

    add 05. node server

---
+++ 05. Node Server/logEvents.js
@@ -0,0 +1,26 @@
+const { v4: uuid } = require("uuid");
+const { format } = require("date-fns");
+
+const path = require("path");
+const fs = require("fs");
+const fsPromises = require("fs").promises;
+
+const logEvents = async (msg, filename) => {
+  const dateTime = `${format(new Date(), "yyyyMMdd\tHH:mm:ss")}`;
+  const logItem = `${dateTime}\t${uuid()}\t${msg}\n`;
+
+  try {
+    if (!fs.existsSync(path.join(__dirname, "logs"))) {
+      await fsPromises.mkdir(path.join(__dirname, "logs"));
+    }
+
+    await fsPromises.appendFile(
+      path.join(__dirname, "logs", filename),
+      logItem
+    );
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+module.exports = logEvents;
